bba48d9da903f728e6d111797b941a903c9d3b23
daniel <danandres281@gmail.com>
2022-08-28 20:34:29 -0500

    Añadiendo imagen del boton back y primer prototipo del primer minijuego

--- Assets/Scripts/DropSlot.cs
+++ Assets/Scripts/DropSlot.cs
@@ -7,22 +7,40 @@ using UnityEngine.EventSystems;
 public class DropSlot : MonoBehaviour , IDropHandler
 {
     public GameObject item;
+    public GameObject cuadrado;
+    public GameObject triangulo;
+    public GameObject circulo;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    public IEnumerator AparecerTriangulo(GameObject triangulo){
+        yield return new WaitForSecondsRealtime(3);
+        triangulo.SetActive(true);
+    }
+     public IEnumerator DesaparecerCuadrado(GameObject cuadrado){
+        yield return new WaitForSecondsRealtime(3);
+        cuadrado.SetActive(false);
+    }
       public void OnDrop(PointerEventData data)
     {
-        if(!item){
+        if(!cuadrado && !item){
+            
+            StartCoroutine(AparecerTriangulo(triangulo));
+            StartCoroutine(DesaparecerCuadrado(item));
+            Debug.Log(item.name);
+            
+        } 
+        /*else if(!triangulo && !item ){
             item = DragHandler.itemDragging;
             item.transform.SetParent(transform);
             item.transform.position = transform.position;
 
-        }
-        
+        }*/
     }
-
+   
     // Update is called once per frame
     void Update()
     {
