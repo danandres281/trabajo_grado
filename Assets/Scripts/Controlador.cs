d9e3837e9fb2f28ac327cf818d1f925308736d81
daniel <danandres281@gmail.com>
2022-09-03 17:44:39 -0500

    Añadiendo validaciones de posiciones de slots de figuras y menu de fin del juego

---
+++ Assets/Scripts/Controlador.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public GameObject triangulo,cuadrado,circulo, slotCirculo, slotCuadrado, slotTriangulo, finJuego;
+    //public GameObject finJuego;
+    Vector2 trianguloInitialPos,cuadradoInitialPos,circuloInitialPos;
+    void Start()
+    {
+        trianguloInitialPos = triangulo.transform.position;
+        cuadradoInitialPos = cuadrado.transform.position;
+        circuloInitialPos = circulo.transform.position;
+        finJuego.SetActive(false);
+        
+    }
+    public IEnumerator AparecerTriangulo(GameObject triangulo){
+        yield return new WaitForSecondsRealtime(2);
+        triangulo.SetActive(true);
+    }
+    public IEnumerator AparecerCirculo(GameObject circulo){
+        yield return new WaitForSecondsRealtime(2);
+        circulo.SetActive(true);
+    }
+     public IEnumerator DesaparecerCuadrado(GameObject cuadrado){
+        yield return new WaitForSecondsRealtime(2);
+        cuadrado.SetActive(false);
+    }
+     public IEnumerator DesaparecerTriangulo(GameObject triangulo){
+        yield return new WaitForSecondsRealtime(2);
+        triangulo.SetActive(false);
+    }
+    public IEnumerator FinalizarJuego(GameObject finJuego){
+        yield return new WaitForSecondsRealtime(2);
+        finJuego.SetActive(true);
+    }
+     public IEnumerator DesaparecerSlotTriangulo(GameObject slotTriangulo){
+        yield return new WaitForSecondsRealtime(2);
+        slotTriangulo.SetActive(false);
+    }
+    public IEnumerator DesaparecerSlotCuadrado(GameObject slotCuadrado){
+        yield return new WaitForSecondsRealtime(2);
+        slotCuadrado.SetActive(false);
+    }
+    public void DragTriangulo(){
+        triangulo.transform.position = Input.mousePosition;
+
+    }
+    public void DragCuadrado(){
+        cuadrado.transform.position = Input.mousePosition;
+        
+         
+    }
+    public void DragCirculo(){
+        circulo.transform.position = Input.mousePosition;
+
+    }
+    public void DropTriangulo(){
+        float distancia = Vector3.Distance(triangulo.transform.position, slotTriangulo.transform.position);
+        if(distancia < 50){
+            triangulo.transform.position = slotTriangulo.transform.position;
+             StartCoroutine(AparecerCirculo(circulo));
+        StartCoroutine(DesaparecerTriangulo(triangulo));
+        }
+        else{
+            triangulo.transform.position = trianguloInitialPos;
+        }
+
+    }
+    public void DropCuadrado(){
+        float distancia = Vector3.Distance(cuadrado.transform.position, slotCuadrado.transform.position);
+        if(distancia < 50){
+            cuadrado.transform.position = slotCuadrado.transform.position;
+            StartCoroutine(AparecerTriangulo(triangulo));
+        StartCoroutine(DesaparecerCuadrado(cuadrado));
+        }
+        else{
+            cuadrado.transform.position = cuadradoInitialPos;
+        }
+        
+
+    }
+    public void DropCirculo(){
+        float distancia = Vector3.Distance(circulo.transform.position, slotCirculo.transform.position);
+        if(distancia < 50){
+            circulo.transform.position = slotCirculo.transform.position;
+            StartCoroutine(FinalizarJuego(finJuego));
+            StartCoroutine(DesaparecerSlotTriangulo(slotTriangulo));
+            StartCoroutine(DesaparecerSlotCuadrado(slotCuadrado));
+            
+        }
+        else{
+            circulo.transform.position = circuloInitialPos;
+        }
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DropTriangulo.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DropTriangulo : MonoBehaviour , IDropHandler
+{
+    //public GameObject item;
+    //public GameObject cuadrado;
+    public GameObject item;
+    public GameObject cuadrado;
+    public GameObject triangulo;
+    public GameObject circulo;
+    //public GameObject circulo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    /*public IEnumerator AparecerTriangulo(GameObject triangulo){
+        yield return new WaitForSecondsRealtime(3);
+        triangulo.SetActive(true);
+    }*/
+     /*public IEnumerator DesaparecerCuadrado(GameObject cuadrado){
+        yield return new WaitForSecondsRealtime(3);
+        cuadrado.SetActive(false);
+    }*/
+      public void OnDrop(PointerEventData data)
+    {
+                  //Debug.Log(triangulo.name);
+       if(!triangulo && !item && cuadrado !=null){
+            item = DragHandler.itemDragging;
+            item.transform.SetParent(transform);
+            item.transform.position = transform.position;
+            
+            //StartCoroutine(AparecerTriangulo(triangulo));
+            //StartCoroutine(DesaparecerCuadrado(item));
+            Debug.Log(item.name);
+            
+        } 
+        /*else if(!triangulo && !item ){
+            item = DragHandler.itemDragging;
+            item.transform.SetParent(transform);
+            item.transform.position = transform.position;
+
+        }*/
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+       if(item != null && item.transform.parent != transform){
+            item = null;
+
+        }
+        
+    }
+   
+}
